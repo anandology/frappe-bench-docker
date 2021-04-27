eac0390be01fc63aa3bdea7312921213d77da0df
Anand Chitipothu <anandology@gmail.com>
2021-04-27 12:18:46 +0530

    Added support for installing custom apps on startup
    This is done by specifying FRAPPE_APPS environment variable

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 
 if [ ! -d "sites/frappe.localtest.me" ]
 then
@@ -11,6 +12,16 @@ then
         --admin-password admin \
         --db-name frappe \
         frappe.localtest.me
+
+    # add a new line as the apps.txt doesn't have an ending new line
+    echo >> sites/apps.txt
+    for app in $FRAPPE_APPS
+    do
+        echo "== installing app $app =="
+        echo $app >> sites/apps.txt
+        (cd apps/$app && ../../env/bin/python setup.py develop)
+        bench --site frappe.localtest.me install-app $app
+    done
 fi
 
 exec bench start
