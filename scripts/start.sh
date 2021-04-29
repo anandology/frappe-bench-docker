c33e3224a1951c8c672cbf5ea9acfca6647f54f0
Anand Chitipothu <anandology@gmail.com>
2021-04-29 13:33:30 +0530

    Allowed customizing the sitename

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 set -e
 
-if [ ! -d "sites/frappe.localtest.me" ]
+SITENAME=${FRAPPE_SITE_NAME:-frappe.localhost}
+
+if [ ! -d "sites/$SITENAME" ]
 then
     echo 'sleeping 15 seconds for mysql to start'
     sleep 15
-    echo creating new site frappe.localtest.me
+    echo creating new site $SITENAME
     bench new-site \
         --db-host mariadb \
         --mariadb-root-password root \
         --admin-password admin \
         --db-name frappe \
-        frappe.localtest.me
+        $SITENAME
 
     # add a new line as the apps.txt doesn't have an ending new line
     echo >> sites/apps.txt
@@ -20,8 +22,14 @@ then
         echo "== installing app $app =="
         echo $app >> sites/apps.txt
         (cd apps/$app && ../../env/bin/python setup.py develop)
-        bench --site frappe.localtest.me install-app $app
+        bench --site $SITENAME install-app $app
     done
+
+    if [ "$FRAPPE_ALLOW_TESTS" == "true" ]
+    then
+        echo "setting allow_tests true"
+        bench --site $SITENAME set-config allow_tests true
+    fi
 fi
 
 exec bench start
